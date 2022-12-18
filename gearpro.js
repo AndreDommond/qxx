e70816f4c855c481c75a5d9c5270d407259921f0
AndreDommond <65059481+AndreDommond@users.noreply.github.com>
2022-12-18 17:00:40 +0800

    Update gearpro.js

--- gearpro.js
+++ gearpro.js
@@ -5,7 +5,7 @@
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\.+ url script-response-body https://raw.githubusercontent.com/AndreDommond/qxx/master/gearpro.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers\/(\$RCAnonymousID%)?([\u4E00-\u9FA5A-Za-z0-9-_]+))$ url script-response-body https://raw.githubusercontent.com/AndreDommond/qxx/master/gearpro.js
 
 [mitm] 
 
