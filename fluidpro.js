a1b5cb40c5ec1ce03df630f4de3d617681f6fda0
AndreDommond <65059481+AndreDommond@users.noreply.github.com>
2022-12-18 20:37:12 +0800

    Update fluidpro.js

--- fluidpro.js
+++ fluidpro.js
@@ -6,7 +6,7 @@
 
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/(\$RCAnonymousID%)?(\w)*$) url script-response-body https://raw.githubusercontent.com/AndreDommond/qxx/master/fluidpro.js
 
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers\/(\$RCAnonymousID%)?([\u4E00-\u9FA5A-Za-z0-9-_]+)) url script-response-body https://raw.githubusercontent.com/AndreDommond/qxx/master/fluidpro.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers\/(\$RCAnonymousID%)?([\u4E00-\u9FA5A-Za-z0-9-_]+)) url script-response-header https://raw.githubusercontent.com/AndreDommond/qxx/master/fluidpro.js
 
 [mitm] 
 
