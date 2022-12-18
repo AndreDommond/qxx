0ea2bbb5398cebf387c730f80bf0e4bdf509e3cc
AndreDommond <65059481+AndreDommond@users.noreply.github.com>
2022-12-19 05:27:05 +0800

    Update 555.js

--- 555.js
+++ 555.js
@@ -5,7 +5,7 @@ TG频道群：https://t.me/gjwj6
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers\/(\$RCAnonymousID%)?([\u4E00-\u9FA5A-Za-z0-9-_]+)) url script-response-body https://raw.githubusercontent.com/AndreDommond/qxx/master/555.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers\/(\$RCAnonymousID%)?([\u4E00-\u9FA5A-Za-z0-9-_]+))$ url script-response-body https://raw.githubusercontent.com/AndreDommond/qxx/master/555.js
 [mitm] 
 hostname = api.revenuecat.com
 *******************************/
@@ -47,7 +47,5 @@ obj = {
                 }
             }
         }
-    };
-}
-;
+};
 $done({ "body": JSON.stringify(obj) });
