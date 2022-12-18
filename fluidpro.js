b0b89d0f4bb70ed03fff7943edf5592462a97180
AndreDommond <65059481+AndreDommond@users.noreply.github.com>
2022-12-19 05:15:58 +0800

    Update fluidpro.js

--- fluidpro.js
+++ fluidpro.js
@@ -1,46 +1,57 @@
+/******************************
+
+
+
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/AndreDommond/qxx/master/fluidpro.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers\/(\$RCAnonymousID%)?([\u4E00-\u9FA5A-Za-z0-9-_]+)) url script-response-body https://raw.githubusercontent.com/AndreDommond/qxx/master/fluidpro.js
+
+
 
 [mitm] 
 
 hostname = api.revenuecat.com
 
 *******************************/
-_0x55b28d = {
-    "request_date": "2022-10-14T06:24:23Z",
-    "request_date_ms": 1665728663210,
-    "subscriber": {
-        "entitlements": {
-            "pro": {
-                "expires_date": "",
-                "grace_period_expires_date": null,
-                "product_identifier": "com.lkzhao.editor.full",
-                "purchase_date": "2022-10-14T06:23:53Z"
-            }
-        },
-        "first_seen": "2022-10-14T06:20:43Z",
-        "last_seen": "2022-10-14T06:20:43Z",
-        "management_url": "https://apps.apple.com/account/subscriptions",
-        "non_subscriptions": {},
-        "original_app_user_id": "$RCAnonymousID:4024fefebf014458bef29f2e013c5124",
-        "original_application_version": "251",
-        "original_purchase_date": "2022-10-14T06:18:56Z",
-        "other_purchases": {},
-        "subscriptions": {
-            "com.lkzhao.editor.full": {
-                "billing_issues_detected_at": null,
-                "expires_date": "",
-                "grace_period_expires_date": null,
-                "is_sandbox": false,
-                "original_purchase_date": "2022-10-14T06:23:55Z",
-                "ownership_type": "PURCHASED",
-                "period_type": "pro",
-                "purchase_date": "2022-10-14T06:23:53Z",
-                "store": "app_store",
-                "unsubscribe_detected_at": null
+var obj =JSON.parse($response.body);
+obj = {
+        "request_date": "2022-12-07T09:56:55Z",
+        "request_date_ms": 1670407015963,
+        "subscriber": {
+            "entitlements": {
+                "premium": {
+                    "expires_date": "2099-12-10T09:56:49Z",
+                    "grace_period_expires_date": null,
+                    "product_identifier": "premium_iap_id",
+                    "purchase_date": "2022-09-22T02:19:32Z"
+                }
+            },
+            "first_seen": "2022-09-22T02:12:43Z",
+            "last_seen": "2022-09-22T02:12:43Z",
+            "management_url": "https://apps.apple.com/account/subscriptions",
+            "non_subscriptions": {},
+            "original_app_user_id": "$RCAnonymousID:AndreDommond",
+            "original_application_version": "1",
+            "original_purchase_date": "2022-09-22T02:12:43Z",
+            "other_purchases": {},
+            "subscriptions": {
+                "premium_iap_id": {
+                    "auto_resume_date": null,
+                    "billing_issues_detected_at": null,
+                    "expires_date": "2099-12-10T09:56:49Z",
+                    "grace_period_expires_date": null,
+                    "is_sandbox": false,
+                    "original_purchase_date": "2022-09-22T02:12:43Z",
+                    "ownership_type": "PURCHASED",
+                    "period_type": "trial",
+                    "purchase_date": "2022-09-22T02:12:43Z",
+                    "refunded_at": null,
+                    "store": "app_store",
+                    "unsubscribe_detected_at": null
+                }
             }
         }
-    }
-};
-$done({ "body": JSON.stringify(_0x55b28d) });
+    };
+}
+;
+$done({ "body": JSON.stringify(obj) });
