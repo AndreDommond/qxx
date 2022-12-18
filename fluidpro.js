744f38a62eb33fb7a0301cb3ec0c2f7bd68f9cde
AndreDommond <65059481+AndreDommond@users.noreply.github.com>
2022-12-19 04:01:45 +0800

    Update fluidpro.js

--- fluidpro.js
+++ fluidpro.js
@@ -14,27 +14,12 @@ hostname = api.revenuecat.com
 
 *******************************/
 var objc = JSON.parse($response.body);
-objc = {
-  "request_date": "2022-12-18T19:38:02Z",
-  "request_date_ms": 1671392282633,
-  "subscriber": {
-    "entitlements": {
-     "premium": {
+objc.subscriber['entitlements'] = "premium": {
                     "expires_date": "2999-12-10T09:56:49Z",
                     "grace_period_expires_date": null,
                     "product_identifier": "premium_iap_id",
-                    "purchase_date": "2022-09-22T02:19:32Z"}
-                    },
-    "first_seen": "2022-12-18T19:31:37Z",
-    "last_seen": "2022-12-18T19:31:37Z",
-    "management_url": null,
-    "non_subscriptions": {},
-    "original_app_user_id": "$RCAnonymousID:2a1dc2162d994c7a8321962de36be41b",
-    "original_application_version": "4000",
-    "original_purchase_date": "2022-12-10T13:59:27Z",
-    "other_purchases": {},
-    "subscriptions": {
-                "premium_iap_id": {
+                    "purchase_date": "2022-09-22T02:19:32Z"};
+objc.subscriber['subscriptions'] = "premium_iap_id": {
                     "auto_resume_date": null,
                     "billing_issues_detected_at": null,
                     "expires_date": "2999-12-10T09:56:49Z",
@@ -47,10 +32,6 @@ objc = {
                     "refunded_at": null,
                     "store": "app_store",
                     "unsubscribe_detected_at": null
-                }
-            }
-  }
-};
-console.log("HHH");
+                };
 $done({ "body": JSON.stringify(objc) });
             
