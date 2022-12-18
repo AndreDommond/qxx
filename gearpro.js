a44e617de0b0e7aa73140870329ff350f1189b4a
AndreDommond <65059481+AndreDommond@users.noreply.github.com>
2022-12-18 16:38:38 +0800

    Add files via upload

---
+++ gearpro.js
@@ -0,0 +1,58 @@
+
+*******************************
+
+
+
+[rewrite_local]
+
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/fileball.js
+
+[mitm] 
+
+hostname = api.revenuecat.com
+
+*******************************/
+
+
+var obj =JSON.parse($response.body);
+obj = {
+  "request_date_ms" : 1663813730272,
+  "request_date" : "2022-09-22T02:28:50Z",
+  "subscriber" : {
+    "last_seen" : "2022-09-22T02:12:43Z",
+    "first_seen" : "2022-09-22T02:12:43Z",
+    "original_application_version" : "322",
+    "other_purchases" : {
+
+    },
+    "management_url" : "https://apps.apple.com/account/subscriptions",
+    "subscriptions" : {
+      "com.olegst.usage.pro" : {
+        "is_sandbox" : false,
+        "ownership_type" : "PURCHASED",
+        "billing_issues_detected_at" : null,
+        "period_type" : "trial",
+        "expires_date" : null,
+        "grace_period_expires_date" : null,
+        "unsubscribe_detected_at" : "2022-09-22T02:21:43Z",
+        "original_purchase_date" : "2022-09-22T02:19:33Z",
+        "purchase_date" : "2022-09-22T02:19:32Z",
+        "store" : "app_store"
+      }
+    },
+    "entitlements" : {
+      "pro" : {
+        "expires_date" : null,
+        "purchase_date" : "2022-09-22T02:19:32Z",
+        "product_identifier" : "com.olegst.usage.pro",
+        "grace_period_expires_date" : null
+      }
+    },
+    "original_purchase_date" : "2022-09-10T12:59:20Z",
+    "original_app_user_id" : "$RCAnonymousID:fe9dc27c6cd44b908576a7bd2eae8e24",
+    "non_subscriptions" : {
+
+    }
+  }
+};
+$done({ "body": JSON.stringify(obj) });
